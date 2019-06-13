778777b5e3c784bf96cae4651b0b0177299f82f2
conconrob <connortprobertson@gmail.com>
2019-06-13 13:14:48 +0100

    Done ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -1,6 +1,12 @@
-// Write a program that calls `fork()`. Before calling `fork()`, have the main process access a variable
-// (e.g., x) and set its value to something (e.g., 100). What value is the variable in the child process?
-// What happens to the variable when both the child and parent change the value of x?
+// Write a program that calls `fork()`. Before calling `fork()`,
+//have the main process access a variable
+// (e.g., x) and set its value to something (e.g., 100).
+// What value is the variable in the child process?
+//  it is the same because it is a copy
+// What happens to the variable when both the child
+//and parent change the value of x?
+// it only changes for the process that is running.
+// since it was changed after the copy it didn't change
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -8,7 +14,27 @@
 
 int main(void)
 {
+    int x = 100;
     // Your code here
-
+    int rc = fork();
+    if (rc == -1)
+    {
+        printf("fork failed");
+        exit(1);
+    }
+    else if (rc == 0)
+    {
+        // child
+        printf("child(%i): %i\n", rc, x);
+        x = 10;
+    }
+    else
+    {
+        // parent
+        waitpid(rc);
+        printf("parent(%i): %i\n", rc, x);
+        x = 50;
+    }
+    printf("rc=%i: %i\n", rc, x);
     return 0;
 }
